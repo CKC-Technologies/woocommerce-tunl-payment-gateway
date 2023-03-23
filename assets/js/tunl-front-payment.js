dad69c5f3b4baa043dafbb793ae8c534a17bf42c
Zach Lankton <zachlankton@gmail.com>
2023-03-23 13:50:13 +0000

    fix front end jquery masking

--- assets/js/tunl-front-payment.js
+++ assets/js/tunl-front-payment.js
@@ -1,7 +1,7 @@
 jQuery(document).ready(function($){
 
 
-    $(document).on("change keyup input","#tunl_ccno",function() { 
+    $(document).on("change input","#tunl_ccno",function() { 
         
         jQuery('.card-input-img').remove();
         var getInputFirst = parseInt($(this).val().charAt(0));
@@ -9,41 +9,39 @@ jQuery(document).ready(function($){
         if( getInputFirst == 3 ){
             imageSet = '<img src="'+cardDetail.cardfolder+'/amex.svg" class="card-input-img">';
             jQuery('#tunl_cvc').val('');
+            jQuery('#tunl_expdate').val('')
             jQuery('#tunl_cvc').unmask().mask("0000"); 
+            jQuery('#tunl_ccno').mask('0000 000000 00000');
         }else if( getInputFirst == 4 ){
             imageSet = '<img src="'+cardDetail.cardfolder+'/visa.svg" class="card-input-img">';
             jQuery('#tunl_cvc').val('');
+            jQuery('#tunl_expdate').val('')
             jQuery('#tunl_cvc').unmask().mask("000"); 
+            jQuery('#tunl_ccno').mask('0000 0000 0000 0000');
         }else if( getInputFirst == 5 ){
             imageSet = '<img src="'+cardDetail.cardfolder+'/mastercard.svg" class="card-input-img">';
             jQuery('#tunl_cvc').val('');
+            jQuery('#tunl_expdate').val('')
             jQuery('#tunl_cvc').unmask().mask("000"); 
+            jQuery('#tunl_ccno').mask('0000 0000 0000 0000');
         }else if( getInputFirst == 6 ){
             imageSet = '<img src="'+cardDetail.cardfolder+'/discover.svg" class="card-input-img">';
             jQuery('#tunl_cvc').val('');
+            jQuery('#tunl_expdate').val('')
             jQuery('#tunl_cvc').unmask().mask("000"); 
+            jQuery('#tunl_ccno').mask('0000 0000 0000 0000');
         }
         jQuery('#tunl_ccno').after(imageSet);
     });
 
-});
+    let mask_initialized = false;
 
-
-
-
-
-
-
-jQuery(window).load(function() {
-
-    setTimeout(() => {
-
+    $(document).on("focus","#tunl_ccno, #tunl_expdate, #tunl_cvc",function() { 
+        if (mask_initialized) return
+        mask_initialized = true
         jQuery('#tunl_ccno').mask('0000 0000 0000 0000');
-
         jQuery('#tunl_expdate').mask('00/00');
-
         jQuery('#tunl_cvc').mask('000');
-
-    }, 4000);
+    });
 
 });
